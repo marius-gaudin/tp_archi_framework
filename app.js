de0308f3bf55da01dee6b9c1f31eff62240e835e
Yessi Munguengui <yessimunguengui@gmail.com>
2021-11-19 13:14:05 +0100

    show filters function

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
 var fs = require("fs");
 
 const start = () => {
-
-  if(checkFilters()) {
-    console.log("filters : ");
-    fs.readdirSync("./filters/").forEach((file) => {
-      console.log(file);
-    });
+  if (checkFilters()) {
+    showFilters();
   }
-  
 };
 
 const checkFilters = () => {
   let isValid = true;
   fs.readdirSync("./filters/").forEach((file) => {
-
     let fc = require(`./filters/${file}`);
 
     if (typeof fc != "function") {
@@ -22,16 +16,24 @@ const checkFilters = () => {
       isValid = false;
       return false;
     }
-     
+
     if (fc.length != 1) {
-      console.error(`La fonction du filtre "${file}" doit contenir un seul argument input`);
+      console.error(
+        `La fonction du filtre "${file}" doit contenir un seul argument input`
+      );
       isValid = false;
       return false;
     }
-     
   });
 
   return isValid;
-}
+};
+
+const showFilters = () => {
+  console.log("filters : ");
+  fs.readdirSync("./filters/").forEach((file) => {
+    console.log(file);
+  });
+};
 
 start();
