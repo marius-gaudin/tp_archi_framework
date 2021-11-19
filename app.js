1575080e1eaec401dede076875918ade97957286
Yessi Munguengui <yessimunguengui@gmail.com>
2021-11-19 11:58:32 +0100

    show filters

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const express = require("express");
 const app = express();
+var fs = require("fs");
+var path = require("path");
+
 app.listen(3000, () => {
   console.log("Server up and running on port 3000");
 });
+
+const start = () => {
+  console.log("filters : ");
+  fs.readdirSync("./filters/").forEach((file) => {
+    console.log(file);
+  });
+};
+
+start();
