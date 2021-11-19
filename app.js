9586da664df18a5d2ae674d991b32650f3e3e026
Marius GAUDIN <g-marius@hotmail.fr>
2021-11-19 14:43:28 +0100

    checkConfig

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ var fs = require("fs");
 const start = () => {
   if (checkFilters()) {
     showFilters();
+    if(checkConfig()){
+      
+    }
   }
 };
 
+// Vérifie que la signature de chaque filter placé dans le dossier /filters est valide (que le
+//   module retourne bien une fonction
 const checkFilters = () => {
   let isValid = true;
   fs.readdirSync("./filters/").forEach((file) => {
@@ -28,6 +33,52 @@ const checkFilters = () => {
 
   return isValid;
 };
+
+const checkConfig = () => {
+
+  try {
+    let ficConfig = fs.readFileSync("config-filters.json");
+    let config = JSON.parse(ficConfig);
+  
+    if(config.hasOwnProperty("steps") && typeof config.steps == "object") {
+      for (const step in config.steps ) {
+        if(step && step!="") {
+
+          if(!config.steps[step].hasOwnProperty("filter")) {
+            throw "Chaque step doit avoir un identifiant";
+          }
+
+          if(config.steps[step].hasOwnProperty("params")) {
+            if(!Array.isArray(config.steps[step].params))
+              throw `Le champ params de la step "${step}"" doit être un array`;
+          }
+
+          if(config.steps[step].hasOwnProperty("next")) {
+            if(config.steps != step && !config.steps.hasOwnProperty(config.steps[step].next))
+              throw `Le paramètre next de la step "${step}" doit corespondre à un identifiant d'une autre step`;
+          }
+
+          return true;
+        } else {
+          throw "Chaque step doit avoir un identifiant";
+        }
+      }
+    } else {
+      throw "Dans le fichier config-filters.json il manque la propriété steps de type Object";
+    }
+
+  } catch(e) {
+
+    if(e.code == "ENOENT") {
+      console.error("Le fichier config-filters.json n'existe pas ! il faut obligatoirement le définir");
+    } else {
+      console.error("Un problème est survenu dans le fichier config-filters.json : ");
+      console.error(e);
+    }
+    return false;
+  }
+  
+}
 
 const showFilters = () => {
   console.log("filters : ");
